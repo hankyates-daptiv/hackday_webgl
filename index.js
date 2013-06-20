88c2127653d3633555db8713064618fd174ac657
Hank Yates <hyates@daptiv.com>
2013-06-20 12:07:09 -0700

    we got numbers and structs into webgl

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var glsl = Glsl({
+function Circle(x, y, radius) {  
+    this.center = { x: x, y: y };  
+    this.radius = radius;  
+    this.originalRadius = radius; // not visible by GLSL  
+}  
+
+Circle.prototype.update = function () {  
+    this.radius = this.originalRadius * Math.sin(Date.now() / 100);
+};
+
+var c1 = new Circle(0.5, 0.5, 0.1);  
+
+Glsl({  
     canvas: document.getElementById("viewport"),
     fragment: document.getElementById("fragment").textContent,
-    variables: {
-        time: 0 // The time in ms
-    },
-    update: function (time) {
-                this.set("time", time);
-            }
+    variables: {  
+        c1: c1  
+    },  
+    update: function (time, delta) {  
+        c1.update();  
+        this.sync("c1");  
+    }  
 }).start();
